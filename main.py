57e881f38ca0bc2256d9d91842ce867ed7ea204d
Er3bCh4os <aksn2008@gmail.com>
2023-06-23 00:08:31 +0700

    Two functions version (untested)

---
+++ main.py
@@ -0,0 +1,64 @@
+import discord
+import random
+from discord.ext import commands
+from config import settings
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
+
+
+@bot.command()
+async def helper(ctx):
+    await ctx.send('Для броска необходимо написать '+settings['prefix']+'r и два числа через пробел')
+    await ctx.send('Первое число - это количество кубов, а второе - сложность броска.')
+    await ctx.send('Пример броска: '+settings['prefix']+'r 2 6')
+    await ctx.send('Для броска на удачу нужно написать ' + settings['prefix'] + 'lu')
+
+
+@bot.command()
+def check_ten(cube: int, dif: int, result: list, pros: list):
+    if cube == 10:  # additional roll
+        result.append(cube)
+        pros.append(cube)
+        check_ten(random.randint(1, 10), result)
+    elif cube >= dif:
+        result.append(cube)
+        pros.append(cube)
+        return
+
+
+async def r(ctx, dice: int, dif: int, result: list, pros: list, cons: list):
+    # result, pros, cons = [], [], []  # initial roll list and +/- lists
+    # count = 0  # cycle counter
+    for i in range(dice):
+        cube = random.randint(1, 10)
+        result.append(cube)
+        # count += 1
+        if cube >= dif:  # difficulty check
+            pros.append(cube)
+        if cube == 1:  # failure check
+            cons.append(1)
+    await ctx.send(result)
+    print(len(cons), len(pros))  # debug info
+    if len(cons) >= 1 and len(pros) == 0:
+        await ctx.send('Провал!')
+    elif len(pros) - len(cons) <= 0:
+        await ctx.send('Неудача!')
+    else:
+        await ctx.send(str(len(pros)-len(cons))+' успеха(ов)!')
+
+
+@bot.command()
+async def lu(ctx):
+    cube = random.randint(1, 10)
+    if cube == 10:
+        await ctx.send('Получилось')
+    elif cube == 1:
+        await ctx.send('Критический провал...')
+    else:
+        await ctx.send('Провал...')
+
+bot.run(settings['token'])
