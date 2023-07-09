bf4e7407fde14f41c6bf64cc342d4f7db7ca1347
Er3bCh4os <37633265+Er3bCh4os@users.noreply.github.com>
2023-07-09 19:30:08 +0700

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'MTExNzcxNDIxNjg1ODY4MTQyNA.GBiCQz.PcZmfZtUEW389D6_M7AvvSroP7_W1tqD1XBkrc',
-    'bot': 'WoD Bot by ErebChaos',
-    'id': 1117714216858681424,
-    'prefix': '$'
-}
